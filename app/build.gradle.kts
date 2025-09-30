eb9ec960acdfa0267f3e63c3e0a9cf1ea469db13
Bigsami89 <143107598+Bigsami89@users.noreply.github.com>
2025-09-30 01:25:09 -0600

    Add JSON data foundation with database manager

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,6 +50,7 @@ dependencies {
     implementation(libs.appcompat)
     implementation(libs.material)
     implementation(libs.constraintlayout)
+    implementation("com.google.code.gson:gson:2.10.1")
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
